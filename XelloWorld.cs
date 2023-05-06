2d7223ad08358d674d9eb3d91fc4237aee512725
SashaGraskov138 <132749249+SashaGraskov138@users.noreply.github.com>
2023-05-06 18:11:54 +0800

    Add files via upload

---
+++ XelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Вывод в консоль приветствия о миру
+        print("hello World"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
